72c03d1c6ad472b4ed88900aaf95daba3b7a78f7
Dmitry <nasdi91@yandex.ru>
2024-07-07 01:29:36 +1000

    Add files via upload

---
+++ fake_math.py
@@ -0,0 +1,9 @@
+def divide(a, b):
+    if b == 0:
+        result = 'Ошибка'
+    else:
+        result = a/b
+    return result
+
+#result = divide(4,0.5)
+#print(result)
---
+++ true_math.py
@@ -0,0 +1,10 @@
+from math import inf
+def divide(a, b):
+    if b == 0:
+        return inf
+    else:
+        result = a/b
+        return result
+
+#result = divide(4,0.5)
+#print(result)
